183ebfd55eb73cffe1fd91caedb9cac1c343f30f
ini3kor <ini3kor@bosch.com>
2020-07-28 16:22:32 +0530

    Initial Commit

---
+++ test/SimpleAPI.Test/SimpleAPI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleAPI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BOSCH")]
+[assembly: AssemblyProduct("SimpleAPI.Test")]
+[assembly: AssemblyCopyright("Copyright © BOSCH 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29b2aa84-e996-461d-a00b-7782add61c8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/SimpleAPI.Test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [TestMethod]
+        public void GetReturnMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.AreEqual("Nikitha Rajeswaran", returnValue);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
